a07695ac2fc524996afe9d0b9b8022f390fedd60
dtxmuramasa <dtx.scv1994@gmail.com>
2025-01-29 05:15:13 +0900

    VCSupportの機能をLRR以外にも展開できるようにした

--- Scripts/BotCogs/VCSupport.py
+++ Scripts/BotCogs/VCSupport.py
@@ -4,38 +4,64 @@ import logging
 
 import os
 from dotenv import load_dotenv
-load_dotenv('.env')
-_LRR_R5_ROLE_ID_ = int(os.environ.get('LRR_R5_ROLE_ID'))
-_LRR_R4_ROLE_ID_ = int(os.environ.get('LRR_R4_ROLE_ID'))
-_LRR_R3_ROLE_ID_ = int(os.environ.get('LRR_R3_ROLE_ID'))
-_LRR_MEDITATION_MODERATOR_ROLE_ID_ = int(os.environ.get('LRR_MEDITATION_MODERATOR_ROLE_ID'))
 
+## 必要な権限
+# ・チャンネル全般の権限 > チャンネルを見る
+# ・チャンネル全般の権限 > チャンネルの管理
+# ・ボイスチャンネル権限 > 接続
+# ・ボイスチャンネル権限 > ボイスチャンネルステータスを設定
 
-class VCSupportCog(commands.Cog):
-    vcs_admin_roles = [
-        _LRR_R5_ROLE_ID_,    # LRR::R5
-        _LRR_R4_ROLE_ID_,    # LRR::R4
-        _LRR_R3_ROLE_ID_,    # LRR::R3
-        _LRR_MEDITATION_MODERATOR_ROLE_ID_,    # LRR::寝落ち警備隊
-    ]
-    
+class VCSupportCog(commands.Cog):    
     def __init__(self, bot: commands.Bot, logger: logging.Logger):
         super().__init__()
         self.bot = bot
         self.logger = logger
+        load_dotenv('.env')
+        self.vcs_admin_roles = []
+        for role_id in os.environ.get('VCS_ADMIN_ROLES').split(','):
+            self.vcs_admin_roles.append(int(role_id))
     
     @commands.Cog.listener()
     async def on_ready(self):
         self.logger.info('VCSupport is ready')
+        
+    async def trySendResponseMessage(self, ctx, message: str, error_message: str) -> bool:
+        try:
+            await ctx.response.send_message(message)
+        except Exception as e:
+            self.logger.error(error_message)
+            return False
+        finally:
+            return True
     
     @app_commands.command()
     async def vcs_setstatus(self, ctx, text: str):
+        server = self.bot.get_guild(ctx.guild_id)
         channel = self.bot.get_channel(ctx.channel_id)
-        if len(list(filter(lambda role: role.id in VCSupportCog.vcs_admin_roles, ctx.user.roles))) <= 0:
+        if len(list(filter(lambda role: role.id in self.vcs_admin_roles, ctx.user.roles))) <= 0:
             self.logger.warning(f'[vcs_setstatus: {channel.name}({channel.id})] called by {ctx.user.display_name}({ctx.user.id}) - unauthorized')
-            await ctx.response.send_message(f'【{ctx.user.display_name}】はこのコマンドを実行する権限がありません')
+            _response_message = f'【{ctx.user.display_name}】はこのコマンドを実行する権限がありません'
+            _error_message = f'[Error]: Require authorities in server:{server.name}({server.id}) channel:{channel.name}({channel.id}) -> SendMessage'
+            await self.trySendResponseMessage(ctx, _response_message, _error_message)
             return
-
-        await channel.edit(status=text)
-        self.logger.info(f'[set_chstatus: {channel.name}({channel.id})] called by {ctx.user.display_name}({ctx.user.id})')
-        await ctx.response.send_message(f'【{ctx.user.display_name}】がチャンネルステータスを設定しました: {text}')
+        
+        _isSucceedChannelEdit = True
+        try:
+            await channel.edit(status=text)
+        except Exception as e:
+            _isSucceedChannelEdit = False
+        finally:
+            pass
+        
+        if not _isSucceedChannelEdit:
+            self.logger.error(f'[Error]: Require authorities in server:{server.name}({server.id}) channel:{channel.name}({channel.id}) -> DiscoverChannel/ManageChannel/ConnectVoiceChannel/EditVoiceChannelStatus')
+            _response_message = f'【{ctx.user.display_name}】がチャンネルステータスを設定できませんでした: {text}\nこの機能ではアプリに対して次の権限が必要です\n - チャンネル全般の権限 > チャンネルを見る\n - チャンネル全般の権限 > チャンネルの管理\n - ボイスチャンネル権限 > 接続\n - ボイスチャンネル権限 > ボイスチャンネルステータスを設定'
+            _error_message = f'[Error]: Require authorities in server:{server.name}({server.id}) channel:{channel.name}({channel.id}) -> SendMessage'
+            if await self.trySendResponseMessage(ctx, _response_message, _error_message):
+                return
+        
+        self.logger.info(f'[vcs_setstatus: {channel.name}({channel.id})] called by {ctx.user.display_name}({ctx.user.id}) -> {text}')
+        _response_message = f'【{ctx.user.display_name}】がチャンネルステータスを設定しました: {text}'
+        _error_message = f'[Error]: Require authorities in server:{server.name}({server.id}) channel:{channel.name}({channel.id}) -> SendMessage'
+        await self.trySendResponseMessage(ctx, _response_message, _error_message)
+        
